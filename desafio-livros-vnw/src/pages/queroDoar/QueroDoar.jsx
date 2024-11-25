4f52d4731bc4bdfc26d5c9ceda0e045938418fc9
robertapdm <robertafarmaco@gmail.com>
2024-11-25 00:32:13 -0300

    creating page Quero Doar e adjusting responsive layout

--- desafio-livros-vnw/src/pages/queroDoar/QueroDoar.jsx
+++ desafio-livros-vnw/src/pages/queroDoar/QueroDoar.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React from "react";
+import s from "./queroDoar.module.scss"
+import Book from "../../assets/images/book.svg"
 
 export default function QueroDoar() {
   return (
-    <div>Quero Doar</div>
-  )
+    <section className={s.container}>
+      <p className={s.info}>
+        Por favor, preencha o formulário com suas informações e as informações
+        do Livro
+      </p>
+      <div className={s.cardForm}>
+        <form className={s.form}>
+          <div className={s.titleForm}>
+            <img src={Book} alt="Ícone de um livro aberto" />
+            <p>Informações do Livro</p>
+          </div>
+          <input type="text" placeholder="Título" />
+          <input type="text" placeholder="Categoria" />
+          <input type="text" placeholder="Autor" />
+          <input type="text" placeholder="Link da imagem" />
+          <button className={s.btn} type="submit">
+            Doar
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 }
